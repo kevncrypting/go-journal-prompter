233577d9265e18d441bdc8ac415c2573f95fba70
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2025-01-02 22:39:46 -0500

    Adds more prompts

--- prompter.go
+++ prompter.go
@@ -10,7 +10,11 @@ func main() {
 		"Did you catch up with a friend today? What did you talk about?",
 		"What did you do for yourself today?",
 		"What do you want to focus on tomorrow?",
+		"What is a challenge I faced recently, and how did I overcome it?",
+		"How can I show myself self-care tomorrow?",
+		"What emotions am I feeling right now?",
+		"What qualities do I want to embody tomorrow?",
 	}
-	
+
 	fmt.Println("Today's journal prompt:", prompts[rand.Intn(len(prompts))])
 }
